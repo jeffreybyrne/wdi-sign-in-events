f1bd9f8187d14c0ea646150e74335d6aeb55ce57
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-27 14:33:07 -0400

    finished the assignment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   const myModal = document.querySelector('.modal');
-  const signInButton = document.querySelector('.signin')
+  const signInButton = document.querySelector('.signin');
   const closeButton = document.querySelector('.close');
+  const submitButton = document.querySelector('.submit');
+  const userField = document.querySelector('#user');
+  const passField = document.querySelector('#pass');
+
   signInButton.addEventListener('click', function() {
     myModal.style.display = 'inline'
   })
@@ -9,6 +13,27 @@ document.addEventListener('DOMContentLoaded', function() {
   closeButton.addEventListener('click', function() {
     if (myModal.style.display != 'none') {
       myModal.style.display = 'none'
+    }
+  })
+
+  submitButton.addEventListener('click', function() {
+    userField.classList.add('error');
+    passField.classList.add('error');
+  })
+
+  userField.addEventListener('click', function() {
+    userField.classList.remove('error');
+  })
+
+  passField.addEventListener('click', function() {
+    passField.classList.remove('error');
+  })
+
+  myModal.addEventListener('click', function(event) {
+    if (myModal == event.target) {
+      if (myModal.style.display != 'none') {
+        myModal.style.display = 'none'
+      }
     }
   })
 });
